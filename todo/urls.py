23ff0275069ec1ace5991a8b365b053623fbdb92
vishalkubde01 <kubdevishal33@gmail.com>
2025-12-23 18:24:05 +0530

    Added few more CRUD Operation

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
   path('addTask/', views.addTask, name='addTask'),
+  path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+  path('mark_as_not_done/<int:pk>/', views.mark_as_not_done, name='mark_as_not_done'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from . models import *
 
@@ -6,4 +6,16 @@ from . models import *
 def addTask(request):
   task = request.POST['task']
   Task.objects.create(task=task)
+  return redirect('home')
+
+def mark_as_done(request, pk):
+  task = get_object_or_404(Task, pk=pk)
+  task.is_completed = True
+  task.save()
+  return redirect('home')
+
+def mark_as_not_done(request,pk):
+  task = get_object_or_404(Task,pk=pk)
+  task.is_completed = False
+  task.save()
   return redirect('home')
